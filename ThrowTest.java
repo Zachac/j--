e584720505a8c0660082e418770fda14a5849868
Zachary Chandler <Zachary.A.Chandler96@gmail.com>
2017-05-17 15:11:26 -0700

    ADDED MISSING REPORTS

--- ThrowTest.java
+++ ThrowTest.java
@@ -1,9 +1,39 @@
 
-import java.lang.NullPointerException;
+import java.lang.IllegalArgumentException;
+import java.lang.System;
 
 public class ThrowTest {
 	
 	public static void main(String[] args) {
-		throw new NullPointerException();
+        
+        System.out.println(false ? false : true);
+        System.out.println(true ? false : true);
+        
+        int i = 0;
+        
+        do {
+            i = i + 1;
+            System.out.println(i);
+        } while (i <= 9);
+        
+        for (i = 10; i > 0; i = i - 1) {
+            System.out.println(i);
+        }
+        
+        if (falseMethod || trueMethod()) {
+            
+        }
+        
+		throw new IllegalArgumentException("exception message");
 	}
+    
+    public static boolean falseMethod() {
+        System.out.println(false);
+        return false;
+    }
+    
+    public static boolean trueMethod() {
+        System.out.println(true);
+        return true;
+    }
 }
--- src/jminusminus/JConditionalOrExpression.java
+++ src/jminusminus/JConditionalOrExpression.java
@@ -1,21 +1,30 @@
 package jminusminus;
 
-public class JConditionalOrExpression extends JBinaryExpression {
+public class JConditionalOrExpression extends JBooleanBinaryExpression {
+	
+	public JConditionalOrExpression(int line, JExpression lhs, JExpression rhs) {
+		super(line, "||", lhs, rhs);
+	}
 
-    public JConditionalOrExpression(int line, JExpression lhs, JExpression rhs) {
-        super(line, "||", lhs, rhs);
-    }
+	public JExpression analyze(Context context) {
+		lhs = (JExpression) lhs.analyze(context);
+        rhs = (JExpression) rhs.analyze(context);
+        lhs.type().mustMatchExpected(line(), Type.BOOLEAN);
+        rhs.type().mustMatchExpected(line(), Type.BOOLEAN);
+        type = Type.BOOLEAN;
+        return this;
+	}
 
-    @Override
-    public JExpression analyze(Context context) {
-        // TODO Auto-generated method stub
-        return null;
-    }
-
-    @Override
-    public void codegen(CLEmitter output) {
-        // TODO Auto-generated method stub
-
-    }
-
+	public void codegen(CLEmitter output, String targetLabel, boolean onTrue) {
+		if (onTrue) { 
+            lhs.codegen(output, targetLabel, true); 
+            rhs.codegen(output, targetLabel, true); 
+            
+        } else { 
+        	String fallThroughLabel = output.createLabel();
+            lhs.codegen(output, fallThroughLabel, true); 
+            rhs.codegen(output, targetLabel, false); 
+            output.addLabel(fallThroughLabel);
+        }
+	}
 }
